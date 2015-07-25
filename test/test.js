d17ab39726434f86c57600b3a7ab4f1dcb87582f
Benjamin Coe <ben@npmjs.com>
2015-07-25 12:53:03 -0700

    finished porting eval tests over to mocha

--- test/test.js
+++ test/test.js
@@ -122,6 +122,7 @@ next = function next(name) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 tests.IPV4 = function () {
     var ipv4addr = process.env.REDIS_PORT_6379_TCP_ADDR || "127.0.0.1";
     var ipv4Client = redis.createClient( PORT, ipv4addr, { family : "IPv4", parser: parser } );
